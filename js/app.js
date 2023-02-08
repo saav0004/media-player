2db4b10b536a67d83e439bebc54761da9ad4c75a
Luciano Saavedra <saav0004@algonquinlive.com>
2023-02-08 18:07:36 -0500

    add time functions

--- js/app.js
+++ js/app.js
@@ -8,7 +8,6 @@ const APP = {
     console.log("llegue a init");
     //called when DOMContentLoaded is triggered
     APP.addListeners();
-    APP.loadCurrentTrack();
     APP.buildPlaylist();
   },
   addListeners: () => {
@@ -24,8 +23,8 @@ const APP = {
     // APP.audio.addEventListener("loadstart", APP.loadstart);
     // APP.audio.addEventListener("loadedmetadata", APP.loadedmetadata);
     // APP.audio.addEventListener("canplay", APP.canplay);
-    // APP.audio.addEventListener("durationchange", APP.durationchange);
-    // APP.audio.addEventListener("timeupdate", APP.timeupdate);
+    APP.audio.addEventListener("durationchange", APP.durationchange);
+    APP.audio.addEventListener("timeupdate", APP.currentTime);
     // APP.audio.addEventListener("play", APP.play);
     // APP.audio.addEventListener("pause", APP.pause);
   },
@@ -48,42 +47,65 @@ const APP = {
     }).join("");
 
     // read the contents of MEDIA and create the playlist
+    let playList = MEDIA.map((music) => (music = music.track));
+    playList.forEach((music) => {
+      APP.tracks.push(music);
+    });
+    // console.log(APP.tracks);
+    APP.loadCurrentTrack();
   },
   loadCurrentTrack: () => {
+    //load function in steve's video
     //use the currentTrack value to set the src of the APP.audio element
-    // APP.audio.src = `./media/${APP.tracks[APP.currentTrack]}`;
+    APP.audio.src = `./media/${APP.tracks[APP.currentTrack]}`;
+    console.log("i am in load current tracks");
     // console.log("Audio has been loaded", APP.audio.src);
   },
   CheckPlayOrPause: () => {
     let playButton = document.getElementById("btnPlay").firstElementChild;
     if (playButton.textContent === "play_arrow") {
       playButton.innerHTML = "pause";
+      APP.play();
     } else {
       playButton.innerHTML = "play_arrow";
+      APP.pause();
     }
   },
   play: () => {
+    if (APP.audio.src) {
+      APP.audio.play();
+    } else {
+      console.warn("You need to load a track first");
+    }
     //start the track loaded into APP.audio playing
   },
   pause: () => {
+    APP.audio && APP.audio.pause();
     //pause the track loaded into APP.audio playing
   },
+  durationchange: (ev) => {
+    console.log(ev.type);
+    let totalTime = document.getElementById("total-time");
+    let convertion = APP.convertTimeDisplay(APP.audio.duration);
+    totalTime.textContent = convertion;
+  },
+  currentTime: () => {
+    let totalTime = document.getElementById("current-time");
+    let convertion = APP.convertTimeDisplay(APP.audio.currentTime);
+    totalTime.textContent = convertion;
+  },
   convertTimeDisplay: (seconds) => {
+    let flooredMinutes = Math.floor(seconds / 60)
+      .toString()
+      .padStart(2, "0");
+    let flooredSeconds = Math.floor(seconds % 60)
+      .toString()
+      .padStart(2, "0");
+
+    return `${flooredMinutes}:${flooredSeconds}`;
+
     //convert the seconds parameter to `00:00` style display
   },
 };
 
 document.addEventListener("DOMContentLoaded", APP.init);
-
-/* <li class="track__item">
-  <div class="track__thumb">
-    <img src="./img/akcent-thumb.png" alt="artist album art thumbnail" />
-  </div>
-  <div class="track__details">
-    <p class="track__title">Przez Twe Oczy Zielone</p>
-    <p class="track__artist">Akcent</p>
-  </div>
-  <div class="track__time">
-    <time datetime="">03:41</time>
-  </div>
-</li>; */
